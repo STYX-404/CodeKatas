980cd2fb427b5c6964bd7ae6c8033d85efa9903b
Ahmed Taha <ahmed.99.tahaa@gmail.com>
2022-04-18 02:35:18 +0200

    Delete [7] Expressions Matter.py

--- [7] Expressions Matter.py
+++
@@ -1,9 +0,0 @@
-def expression_matter(a, b, c):
-    p1 = a * (b + c)
-    p2 = a + b * c
-    p3 = a * b * c
-    p4 = (a + b) * c
-    p5 = a + b + c
-    results = [p1,p2,p3,p4,p5]
-    results.sort()
-    return results.pop()
